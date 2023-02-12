40ad56ba61cc360e4d9930d776b99a61107cbd00
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-02-12 19:56:02 +0200

    Update BlurImage.tsx

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -11,24 +11,22 @@ type BlurImage = {
 	imageSrc: string;
 };
 
-export function cn(...classes: string[]) {
+function cn(...classes: string[]) {
 	return classes.filter(Boolean).join(' ');
 }
 
-export function BlurImage(image: BlurImage) {
+function BlurImage(image: BlurImage) {
 	const [isLoading, setIsLoading] = useState(true);
 
 	return (
 		<a href={image.href} className="group">
-			<div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+			<div className="border-2 border-gray-200 shadow aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
 				<Image
 					src={image.imageSrc}
 					alt={image.name}
 					className={cn(
 						'group-hover:opacity-75 duration-700 ease-in-out',
-						isLoading
-							? 'grayscale blur-2xl scale-110'
-							: 'grayscale-0 blur-0 scale-100'
+						isLoading ? 'grayscale blur-2xl scale-110' : 'grayscale-0 blur-0 scale-100'
 					)}
 					layout="fill"
 					objectFit="cover"
@@ -40,3 +38,5 @@ export function BlurImage(image: BlurImage) {
 		</a>
 	);
 }
+
+export default BlurImage;
