8857ff857755a75b18ed35a8db8d4f8593ebc8cd
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-02-14 01:10:10 +0200

    Fix the bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import supabase from '@/utils/supabase';
 import BlurImage from '@/components/BlurImage';
-import ImageDialog from '@/components/ImageDialog';
+import DialogDemo from '@/components/Dialog';
 
 export type Image = {
 	id: number;
@@ -28,8 +28,8 @@ const Gallery = async () => {
 	return (
 		<section className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 			<div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-				<ImageDialog />
-				{images.map((image) => <BlurImage key={image.id} {...image} />)}
+				<DialogDemo />
+				{images && images.map((image) => <BlurImage key={image.id} {...image} />)}
 			</div>
 		</section>
 	);
--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -1,48 +1,113 @@
-
+'use client';
 import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import { Cross2Icon } from '@radix-ui/react-icons';
-import './styles.css';
 
-const DialogDemo = () => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <button className="Button violet">
-        Edit profile
-      </button>
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="DialogOverlay" />
-      <Dialog.Content className="DialogContent">
-        <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
-        <Dialog.Description className="DialogDescription">
-          Make changes to your profile here. Click save when you re done.
-        </Dialog.Description>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="name">
-            Name
-          </label>
-          <input className="Input" id="name" defaultValue="Pedro Duarte" />
-        </fieldset>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="username">
-            Username
-          </label>
-          <input className="Input" id="username" defaultValue="@peduarte" />
-        </fieldset>
-        <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
-          <Dialog.Close asChild>
-            <button className="Button green">Save changes</button>
-          </Dialog.Close>
-        </div>
-        <Dialog.Close asChild>
-          <button className="IconButton" aria-label="Close">
-            <Cross2Icon />
-          </button>
-        </Dialog.Close>
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-);
+import { useForm } from 'react-hook-form';
+import supabase from '@/utils/supabase';
+
+const DialogDemo = () => {
+	return (
+		<Dialog.Root>
+			<Dialog.Trigger asChild>
+				<button className="Button violet">Add vision</button>
+			</Dialog.Trigger>
+			<Dialog.Portal>
+				<Dialog.Overlay className="DialogOverlay" />
+				<Dialog.Content className="DialogContent">
+					<Dialog.Title className="DialogTitle">Add vision</Dialog.Title>
+					<Dialog.Description className="DialogDescription">
+						Add a vision to your vision board here. Click save when you&apos;re done.
+					</Dialog.Description>
+					<ImageForm />
+					<Dialog.Close asChild>
+						<button className="IconButton" aria-label="Close">
+							<Cross2Icon />
+						</button>
+					</Dialog.Close>
+				</Dialog.Content>
+			</Dialog.Portal>
+		</Dialog.Root>
+	);
+};
 
 export default DialogDemo;
+
+const ImageForm = () => {
+	const { register, handleSubmit } = useForm();
+	const onSubmit = async (data: any) => {
+		const { title, description, category, image } = data;
+		await supabase.from('visions').insert({ title: title, description: description, category: category });
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<fieldset className="Fieldset">
+				<label className="Label" htmlFor="title">
+					Title
+				</label>
+				<input className="Input" {...register('title')} defaultValue="title 1" />
+			</fieldset>
+			<fieldset className="Fieldset">
+				<label className="Label" htmlFor="description">
+					Description
+				</label>
+				<input className="Input" {...register('description')} defaultValue="desc 1" />
+			</fieldset>
+			<fieldset className="Fieldset">
+				<label className="Label" htmlFor="category">
+					Category
+				</label>
+				<select className="Input" {...register('category')}>
+					<option>Family</option>
+					<option>Work</option>
+					<option>Friends</option>
+					<option>Other</option>
+				</select>
+			</fieldset>
+			<fieldset className="Fieldset">
+				<label className="Label">Image</label>
+				<div className="flex-1 mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+					<div className="space-y-1 text-center">
+						<svg
+							className="mx-auto h-12 w-12 text-gray-400"
+							stroke="currentColor"
+							fill="none"
+							viewBox="0 0 48 48"
+							aria-hidden="true">
+							<path
+								d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+								strokeWidth={2}
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</svg>
+						<div className="flex text-sm text-gray-600">
+							<label
+								htmlFor="file-upload"
+								className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
+								<span>Upload a file</span>
+								<input
+									id="file-upload"
+									type="file"
+									className="sr-only"
+									accept="image/*"
+									{...register('image')}
+								/>
+							</label>
+							<p className="pl-1">or drag and drop</p>
+						</div>
+						<p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+					</div>
+				</div>
+			</fieldset>
+			<div className="flex justify-end mt-5">
+				<button
+					type="submit"
+					className="bg-gradient-to-r from-green-600 to-green-800 px-4 py-1.5 rounded text-white shadow">
+					Save changes
+				</button>
+			</div>
+		</form>
+	);
+};
