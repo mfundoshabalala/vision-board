8edf94ff9a85e0c712e866ed34e3c8069c2111a9
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-02-18 13:39:10 +0200

    Update NoVision.tsx

--- components/NoVision.tsx
+++ components/NoVision.tsx
@@ -6,7 +6,9 @@ const NoVision = () => {
 			<div className="">
 				<h1 className="text-2xl font-bold">No Vision</h1>
 				<p className=""></p>
-				<button className='px-4 py-2 text-sm font-medium border rounded shadow'>Create board</button>
+				<button className="px-4 py-2 text-sm font-medium border rounded shadow">
+					Create board
+				</button>
 			</div>
 		</div>
 	);
